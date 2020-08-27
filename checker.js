f8fa77d80bed120e6a2830a09efb81c7f5d64725
Nadimpalli <vinay.n.varma189@gmail.com>
2020-08-27 20:17:29 +0530

    added test cases to cover all combinations

--- checker.js
+++ checker.js
@@ -8,4 +8,18 @@ function vitalsAreOk(bpm, spo2, respRate) {
 expect(vitalsAreOk(100, 95, 70)).to.be.true;
 expect(vitalsAreOk(50, 95, 70)).to.be.false;
 
+expect(vitalsAreOk(20, 95, 70)).to.be.false; // isBPMOk fails
+expect(vitalsAreOk(75, 55, 70)).to.be.false; // isSpo2Ok fails
+expect(vitalsAreOk(75, 95, 25)).to.be.false; // isRespRateOk fails
+
+expect(vitalsAreOk(20, 55, 70)).to.be.false; // isBPMOk and isSpo2Ok fail
+expect(vitalsAreOk(20, 95, 25)).to.be.false; // isBPMOk and isRespRateOk fail
+expect(vitalsAreOk(75, 55, 25)).to.be.false; // isSpo2Ok and isRespRateOk fail
+
+expect(vitalsAreOk(20, 55, 25)).to.be.false; // isBPMOk and isSpo2Ok and isRespRateOk fail
+
+
+
+
+
 console.log('checker is done');
